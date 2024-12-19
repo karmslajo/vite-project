7e8c659d58509ba57d1a93cd24fca08ff447caa9
3LL4N <jetdieron01@gmail.com>
2024-12-19 18:45:15 +0800

    update @types/react to 18.3.17 and enhance App component for OS detection and app redirection

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,72 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import "./App.css";
 
 function App() {
-  const handleClick = () =>
-    (window.location.href =
-      "gumpapp://pia.gump-staging.net/album/alex/20241118kawagoe");
+  function detectOperatingSystem() {
+    const userAgent = navigator.userAgent;
 
-  return <button onClick={handleClick}>Go to App</button>;
+    if (/win/i.test(userAgent)) {
+      return "windows";
+    }
+
+    if (/macintosh|mac os x/i.test(userAgent)) {
+      return "macos";
+    }
+
+    if (/linux/i.test(userAgent)) {
+      return "linux";
+    }
+
+    if (/android/i.test(userAgent)) {
+      return "android";
+    }
+
+    if (/iPad|iPhone|iPod/.test(userAgent)) {
+      return "ios";
+    }
+
+    return "unknown";
+  }
+
+  function redirectToApp(albumUrl: string | null) {
+    const os = detectOperatingSystem();
+
+    if (!albumUrl) return;
+    const appUrl = albumUrl.replace(/^https?:\/\//, "gumpapp://");
+
+    const storeUrl = {
+      ios: "https://apps.apple.com/app/idYOUR_APP_ID",
+      android:
+        "https://play.google.com/store/apps/details?id=YOUR_PACKAGE_NAME",
+    };
+
+    // Create a hidden iframe to attempt to open the app
+    const iframe = document.createElement("iframe");
+    iframe.style.display = "none";
+    iframe.src = appUrl;
+    document.body.appendChild(iframe);
+    console.log("iframe", iframe);
+
+    // Fallback to app store after a delay if the app isn't installed
+    setTimeout(() => {
+      document.body.removeChild(iframe);
+
+      window.location.href = storeUrl["android"];
+      console.log("Redirecting to app store");
+    }, 1500);
+  }
+
+  return (
+    <button
+      onClick={() =>
+        redirectToApp(
+          "gumpapp://pia.gump-staging.net/album/alex/20241118kawagoe"
+        )
+      }
+    >
+      Go to App
+    </button>
+  );
 }
 
 export default App;
