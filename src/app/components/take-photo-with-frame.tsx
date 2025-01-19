755381f46ec49ac87c2d1dc85b15bc3c07cd3864
3LL4N <jetdieron01@gmail.com>
2025-01-19 22:32:32 +0800

    update

--- src/app/components/take-photo-with-frame.tsx
+++ src/app/components/take-photo-with-frame.tsx
@@ -169,6 +169,8 @@ function Camera(props: CameraProps) {
     const constraints = {
       video: {
         audio: false,
+        width: { ideal: 320 },
+        height: { ideal: 250 },
         facingMode: facingMode,
       },
     };
