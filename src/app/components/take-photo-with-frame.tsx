78e967f01240e1d29c9613e8d62bba38c9fa884d
3LL4N <jetdieron01@gmail.com>
2025-02-09 20:05:41 +0800

    update

--- src/app/components/take-photo-with-frame.tsx
+++ src/app/components/take-photo-with-frame.tsx
@@ -240,8 +240,8 @@ function Camera(props: CameraProps) {
         // Other devices would overflow the container if aspect ratio is not set
         aspectRatio: { ideal: 4 / 3 },
         // Adjusted for 8K 4:3 resolution so the browser will pick the highest resolution available
-        width: { ideal: isLandscape ? 5760 : 4320 },
-        height: { ideal: isLandscape ? 4320 : 5760 },
+        width: { ideal: isLandscape ? 5680 : 4260 },
+        height: { ideal: isLandscape ? 4260 : 5680 },
       },
     };
 
