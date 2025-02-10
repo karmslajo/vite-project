6ea8dd0e45e99dcd3c69faa6460b5b13d1a78fd2
3LL4N <jetdieron01@gmail.com>
2025-02-10 13:57:38 +0800

    update camera resolution settings to enforce maximum dimensions for improved compatibility

--- src/app/components/take-photo-with-frame.tsx
+++ src/app/components/take-photo-with-frame.tsx
@@ -245,8 +245,8 @@ function Camera(props: CameraProps) {
         // Other devices would overflow the container if aspect ratio is not set
         aspectRatio: { ideal: 4 / 3 },
         // Adjusted for 8K 4:3 resolution so the browser will pick the highest resolution available
-        width: { ideal: isLandscape ? idealWidth : idealHeight },
-        height: { ideal: isLandscape ? idealHeight : idealWidth },
+        width: { max: 4096, ideal: isLandscape ? idealWidth : idealHeight },
+        height: { max: 4096, ideal: isLandscape ? idealHeight : idealWidth },
       },
     };
 
